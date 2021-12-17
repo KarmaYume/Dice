7f291cf71a0aad85b00931cce0b5f297a14ddb0f
Dante-cyber <59571754+Dante-cyber@users.noreply.github.com>
2021-12-17 12:45:19 +0530

    written an unit test

--- app/src/test/java/app/one/randomnumber/ExampleUnitTest.kt
+++ app/src/test/java/app/one/randomnumber/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package app.one.randomnumber
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +9,10 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
+   @Test
+   fun generates_number() {
+       val dice = Dice(6)
+       val output = dice.roll()
+       assertTrue("The number is not in between 1 and 6" , output in 1..6)
+   }
 }
